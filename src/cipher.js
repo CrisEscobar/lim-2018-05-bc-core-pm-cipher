7441db4aebb9dec1a2d82df94f938a6ed4f8e9c7
CrisEscobar <crisescobar0@hotmail.com>
2018-05-31 18:34:56 -0500

    penultimos cambios

--- src/cipher.js
+++ src/cipher.js
@@ -1,21 +1,48 @@
 window.cipher = {
-    encode: (offset,string) => {
-      var result = "";
-      for (let i = 0; i < string.length; i++) {
-        var letter = (string.charCodeAt(i) - 65 + offset) % 26 + 65;
-        var lettercoded = String.fromCharCode(letter)
-        result = result + lettercoded
+  encode: (offset, string) => {
+    var result = "";
+    for (let i = 0; i < string.length; i++) {
+      let actual = string.charCodeAt(i);
+      if (actual == 32) {
+        result = result + String.fromCharCode(actual);
+        continue
       }
-      return result;
-    },
-    decode: (offset,string) => {
-        var result = "";
-        for (let i = 0; i < string.length; i++) {
-          var letter = ((string.charCodeAt(i) - 65 - offset) % 26 + 65);
-          var lettercoded = String.fromCharCode(letter)
-          result = result + lettercoded
+      if (actual >= 65 && actual <= 90) { //MAYUSCULAS
+        let codigo = (actual - 65 + offset) % 26 + 65;
+        let letra = String.fromCharCode(codigo);
+ 
+        result = result + letra;
+      } else {
+        if (actual >= 97 && actual <= 122) { //MINUSCULAS
+          let codigo = (actual - 97 + offset) % 26 + 97;
+          let letra = String.fromCharCode(codigo);
+          result = result + letra
         }
-        return result;
-       }
+      }
     }
+    return result;
+  },
+  decode: (offset, string) => {
+    var result = "";
+    for (let i = 0; i < string.length; i++) {
+      let actual = string.charCodeAt(i);
+      if (actual == 32) {
+        result = result + String.fromCharCode(actual);
+        continue
+      }
+      if (actual >= 65 && actual <= 90) { //MAYUSCULAS
+        let codigo = (actual - 90 - offset) % 26 + 90;  //cambiando 65 x90
+        let letra = String.fromCharCode(codigo);
+        result = result + letra;
+      } else {
+        if (actual >= 90 && actual <= 122) { //MINUSCULAS
+          let codigo = (actual - 122 - offset) % 26 + 122;
+          let letra = String.fromCharCode(codigo);
+          result = result + letra //invoco a la variable result y le añado la letra
+        }
+      }
+    }
+    return result; //retorna el resultado 
+  }
+ }
  
--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
+document.getElementById("botoncifrar").addEventListener("click", function () {
 
-let cifrar = () => {
     let string = document.getElementById("valor").value;
     let offset = parseInt(document.getElementById("espacio").value);
-    let result = window.cipher.encode(offset,string);
+    let result = window.cipher.encode(offset, string);
     let textoHtml = document.getElementById("cuadroresultado");
     textoHtml.value = result
-   }
-   
-   let descifrar = () => {
+});
+document.getElementById("botondescifrar").addEventListener("click", function () {
     let string = document.getElementById("valordos").value;
     let offset = parseInt(document.getElementById("espaciodos").value);
-    let result = window.cipher.decode(offset,string);
+    let result = window.cipher.decode(offset, string);
     let textoHtml = document.getElementById("cuadroresultadodos");
     textoHtml.value = result
-   }
-   
+});
+document.getElementById("reseteocifrar").addEventListener("click", function () {
+    let textoHtml = document.getElementById("cuadroresultado");
+    textoHtml.value = "" 
+    let textoHtmldescifrar = document.getElementById("valor");
+    textoHtmldescifrar.value = ""
+    let offset = document.getElementById("espacio");
+    offset.value = ""
+});
+document.getElementById("reseteodescifrar").addEventListener("click", function () {
+    let textoHtml = document.getElementById("cuadroresultadodos");
+    textoHtml.value = "" 
+    let textoHtmldescifrar = document.getElementById("valordos");
+    textoHtmldescifrar.value = ""
+    let offset = document.getElementById("espaciodos");
+    offset.value = ""
+});
